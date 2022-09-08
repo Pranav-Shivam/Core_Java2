ba7aabfbdd9093fb9aa2fa28864b3bfc8298d856
Pranav_Shivam <21MCA2618@cuchd.in>
2022-09-08 12:34:54 +0530

    Initial Commit

---
+++ src/Pratice/ListNode.java
@@ -0,0 +1,22 @@
+package Pratice;
+
+public class ListNode {
+    public static void main(String[] args) {
+
+    }
+
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
---
+++ src/Pratice/ReverseLinkedList.java
@@ -0,0 +1,17 @@
+package Pratice;
+
+public class ReverseLinkedList {
+    public static void main(String[] args) {
+
+    }
+    public ListNode reverseList(ListNode head) {
+        ListNode newHead = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = newHead;
+            newHead = head;
+            head = next;
+        }
+        return newHead;
+    }
+}
